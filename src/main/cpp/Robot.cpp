48920feebf73960a6fafbda5d63d0505182c80b0
Nathan Correa <correanathanh@gmail.com>
2024-01-02 11:49:29 -0500

    removed random comments from Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,31 +8,14 @@
 
 void Robot::RobotInit() {}
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want to run during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
 void Robot::RobotPeriodic() {
   frc2::CommandScheduler::GetInstance().Run();
 }
 
-/**
- * This function is called once each time the robot enters Disabled mode. You
- * can use it to reset any subsystem information you want to clear when the
- * robot is disabled.
- */
 void Robot::DisabledInit() {}
 
 void Robot::DisabledPeriodic() {}
 
-/**
- * This autonomous runs the autonomous command selected by your {@link
- * RobotContainer} class.
- */
 void Robot::AutonomousInit() {
   m_autonomousCommand = m_container.GetAutonomousCommand();
 
@@ -50,14 +33,8 @@ void Robot::TeleopInit() {
   }
 }
 
-/**
- * This function is called periodically during operator control.
- */
 void Robot::TeleopPeriodic() {}
 
-/**
- * This function is called periodically during test mode.
- */
 void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
